f7dca1039df1cd8c6139223f2f4784d9d3bfabea
Kearston Fortner <kearstonnicole448@gmail.com>
2023-06-05 13:45:47 -0500

    final push

--- data/selling-sunset_script.sql
+++ data/selling-sunset_script.sql
@@ -39,17 +39,54 @@ app_store_apps.price::money, app_store_apps.primary_genre, play_store_apps.ratin
 ORDER BY app_store_apps.review_count::numeric DESC;
 
 --3B:
-WITH app_trader AS (SELECT *,
-	CASE WHEN price = 0 THEN 25000::MONEY 
-	ELSE price * 10000::MONEY
-	END AS purchase_price
-	FROM app_store_apps)
-SELECT install_count::numeric,
-CASE WHEN review_count::numeric * price::numeric >= purchase_price THEN 'Y' ELSE 'N' END profitable,
-	CASE WHEN price = '0' THEN '25000' 
-	ELSE price::MONEY * 10000
-	END AS purchase_price
-	FROM play_store_apps
-	ORDER BY purchase_price DESC;
-
+WITH top_10 AS ((WITH top_10 AS(SELECT name, price::money AS price, review_count,
+							CASE WHEN price = 0.00 THEN 25000::money
+							ELSE (price * 10000)::money
+							END AS purchase_price
+			   FROM app_store_apps)
+SELECT *,
+CASE WHEN review_count::int * price >= purchase_price THEN 'Y' ELSE 'N' END AS profitable
+FROM top_10
+ORDER BY profitable DESC
+LIMIT 10)
+			    UNION
+(WITH play_store AS(SELECT name, price::money,install_count,
+							CASE WHEN price = '0' THEN '25000'
+							ELSE (price::money * 10000)
+							END AS purchase_price
+							FROM play_store_apps)
+SELECT *,
+CASE WHEN install_count::Money::Numeric*price::money >= purchase_price THEN 'Y' ELSE 'N' END AS profitable
+FROM play_store))
+SELECT *
+FROM top_10
+WHERE profitable = 'Y';
+	--3C:
+WITH top_4_USA AS ((WITH app_store AS(SELECT name, price::money AS price, review_count,
+							CASE WHEN price = 0.00 THEN 25000::money
+							ELSE (price * 10000)::money
+							END AS purchase_price,
+						    review_count::numeric * price::money AS profit
+			   FROM app_store_apps)
+SELECT *,
+CASE WHEN review_count::int * price >= purchase_price THEN 'Y' ELSE 'N' END AS profitable
+FROM app_store
+ORDER BY profitable DESC)
+			    UNION
+(WITH play_store AS(SELECT name, price::money,install_count,
+							CASE WHEN price = '0' THEN '25000'
+							ELSE (price::money * 10000)
+							END AS purchase_price,
+							install_count::money::numeric * price::money AS profit
+							FROM play_store_apps)
+SELECT *,
+CASE WHEN install_count::Money::Numeric*price::money >= purchase_price THEN 'Y' ELSE 'N' END AS profitable
+FROM play_store))
+SELECT *
+FROM top_4_USA
+WHERE profitable = 'Y'
+AND name ILIKE '%America%' OR name ILIKE '%usa%' OR name ILIKE '%WW%'
+ORDER BY profit DESC
+LIMIT 4;
+	
 
